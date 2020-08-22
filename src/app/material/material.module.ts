1b15cedc300174458365756f4609f4d02295b2a9
Akarsh <akarshs27@gmail.com>
2020-08-22 16:48:36 +0530

    Material Theme added

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+const MaterialComponent = [
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialComponent],
+  exports: [MaterialComponent]
+})
+export class MaterialModule { }
