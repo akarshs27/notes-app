bb3172d26ae034d7f4ee0388fb7ddd2d50acfe71
Akarsh <akarshs27@gmail.com>
2020-08-23 00:44:33 +0530

    Initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './pages/note-card/note-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 const MaterialComponent = [
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
